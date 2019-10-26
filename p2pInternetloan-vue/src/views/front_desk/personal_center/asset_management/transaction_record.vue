a81f53dea752dd32a3d88e4ddf3d5bca87b1b709
1298670100@qq.com <1298670100@qq.com>
2019-10-26 21:44:13 +0800

    谭总提交

--- p2pInternetloan-vue/src/views/front_desk/personal_center/asset_management/transaction_record.vue
+++ p2pInternetloan-vue/src/views/front_desk/personal_center/asset_management/transaction_record.vue
@@ -1,26 +1,27 @@
 <template>
   <el-tabs value="first" editable addable closable type="border-card">
     <el-tab-pane name="first" label="充值记录">
-      <el-row>
-        <el-col style="font-weight: bold;" :span="12">充值交易ID：
-          <el-input style="height: 0px; width: 58%;" placeholder="请输入内容" size="medium" clearable show-word-limit type="text"></el-input>
-          <el-button style="right: 275px; margin-right: -50px;" :autofocus="true" plain type="success" :native-type="submit">查询</el-button>
-        </el-col>
-        <el-col style="font-weight: bold;" :span="12">充值交易时间：
-          <el-input style="height: 6px; width: 58%;" placeholder="请输入内容" size="medium" clearable show-word-limit type="text"></el-input>
-          <el-button style="right: 275px; margin-right: -50px;" :autofocus="true" plain type="success" :native-type="submit">查询</el-button>
-        </el-col>
-      </el-row>
+      <el-form class="demo-form-inline" style="margin-top: 10px;background:#FFFFFF;padding-top: 10px; height: 50px;padding-left: 10px;" :inline="true">
+        <el-row>
+          <el-col style="font-weight: bold;" :span="12">充值交易ID：
+            <el-input style="height: 0px; width: 58%;"  v-model="queryParams.id" placeholder="请输入内容" size="medium" clearable show-word-limit type="text"></el-input>
+          </el-col>
+          <el-col style="font-weight: bold;" :span="12">充值交易时间：
+            <el-input style="height: 6px; width: 58%;" v-model="queryParams.trade_time" placeholder="请输入内容" size="medium" clearable show-word-limit type="text"></el-input>
+            <el-button style="right: 275px; margin-right: -50px;" :autofocus="true" plain type="success" :native-type="submit">查询</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
     </el-tab-pane>
     <el-tab-pane name="second" label="提现记录">
       <el-row>
         <el-col style="font-weight: bold;" :span="12">提现交易ID：
           <el-input style="height: 0px; width: 58%;" placeholder="请输入内容" size="medium" clearable show-word-limit></el-input>
-          <el-button style="right: 275px; margin-right: -50px;" :autofocus="true" plain type="success" :native-type="submit">查询</el-button>
+
         </el-col>
         <el-col style="font-weight: bold;" :span="12">提现交易时间：
           <el-input style="height: 6px; width: 58%;" placeholder="请输入内容" size="medium" clearable show-word-limit></el-input>
-          <el-button style="right: 275px; margin-right: -50px;" :autofocus="true" plain type="success" :native-type="submit">查询</el-button>
+          <el-button style="right: 275px; margin-right: -50px;" :autofocus="true" plain type="success"  @click="onQuery">查询</el-button>
         </el-col>
       </el-row>
     </el-tab-pane>
@@ -28,14 +29,14 @@
   <hr>
   </hr>
   <!-- 分页 -->
-  <el-table style="margin-bottom: 43px; padding-top: 0px; margin-left: 0px;" :data="dictList" height="440" :fit="true" :show-header="true" v-loading="loading" show-header="true" highlight-current-row>
+  <el-table :data="usertList" :show-header="true" v-loading="loading" style="margin-bottom: 43px; padding-top: 0px; margin-left: 0px;" :data="dictList" height="440" :fit="true" :show-header="true" v-loading="loading" show-header="true" highlight-current-row>
     <el-table-column prop="id" label="ID" min-width="1.5">
     </el-table-column>
-    <el-table-column prop="title" label="支付宝唯一标识（账号）" min-width="4">
+    <el-table-column prop="trade_code" label="支付宝唯一标识（账号）" min-width="4">
     </el-table-column>
-    <el-table-column prop="sn" label="交易时间" min-width="4">
+    <el-table-column prop="trade_time" label="交易时间" min-width="4">
     </el-table-column>
-    <el-table-column prop="description" label="交易金额" min-width="4">
+    <el-table-column prop="amount" label="交易金额" min-width="4">
     </el-table-column>
     <el-table-column label="操作" min-width="4">
       <template slot-scope="scope">
@@ -55,7 +56,7 @@
 
         },
       methods:{
-          
+
       }
     }
 </script>
